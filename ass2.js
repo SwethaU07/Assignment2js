e9aade76a4b860418fba160f6cf36eb2814df5ff
SwethaU07 <112627519+SwethaU07@users.noreply.github.com>
2022-10-14 12:30:05 +0530

    Add files via upload

---
+++ ass2.js
@@ -0,0 +1,12 @@
+document.write("Assignment-2");
+function pyramid(){
+    for(var i=1;i<=8;i++)
+    {
+        for(var j=1;j<=i;j++)
+        {
+            console.log(i);
+        }
+        console.log("\n");
+    }
+}
+pyramid();
